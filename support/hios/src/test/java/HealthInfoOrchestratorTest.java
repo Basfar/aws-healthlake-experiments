4f41a2862f1b8b6fff5b6de070c5828f53b902f6
Mukhtar Onifade <mukhtaronif@gmail.com>
2024-08-16 23:45:21 +0100

    hios-java

--- support/hios/src/test/java/HealthInfoOrchestratorTest.java
+++ support/hios/src/test/java/HealthInfoOrchestratorTest.java
@@ -11,13 +11,13 @@ public class HealthInfoOrchestratorTest {
     @Test
     void testOrchestrateWithAllFileTypes() throws IOException {
         final var orchestrator = new HealthInfoOrchestrator()
-                .withContentStore(new HealthInfoOrchestrator.AwsS3ContentStore("javahios","us-east-2"))
-                .withFhirStore(new HealthInfoOrchestrator.AwsHealthLakeFhirStore("us-east-2",
-                        "5e89353b17a720c1aa6a6c66a02e880c",
-                        "javahios","arn:aws:kms:us-east-2:992382710407:key/4faf8e6c-ce8a-45a8-8e81-dcb7c662fefb","arn:aws:iam::992382710407:role/service-role/AWSHealthLake-Import-ServiceRole"));
+                .withContentStore(new HealthInfoOrchestrator.AwsS3ContentStore("",""))
+                .withFhirStore(new HealthInfoOrchestrator.AwsHealthLakeFhirStore("",
+                        "",
+                        "","",""));
 
 
-        final var fhirBundleFile = new File("src/test/resources/synthea_sample_data_fhir_latest/Ángela136_Cortez851_d2e56049-1c7c-45ac-5d87-1b81dc9db112.json");
+        final var fhirBundleFile = new File("");
         final var fhirBundle = VFS.getManager().toFileObject(fhirBundleFile);
 
         orchestrator.withFhirBundle(fhirBundle);
